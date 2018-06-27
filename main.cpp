5bb5ec6c59a8e2b5ae883d734877bc620a592306
mihai.pop <mihai.pop@hsae.eu>
2018-06-27 11:35:29 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "foo.h"
 
+typedef int(*fn_handler)(int, int);
+typedef struct {
+  fn_handler handler;
+} t_str;
+
 int main(){
   // A a;
   int sum = 0;
   int t1 = 1;
 
-  sum = add(t1, sum);
+  t_str str {
+    add
+  };
+
+  sum = str.handler(t1, sum);
   return sum;
 }
